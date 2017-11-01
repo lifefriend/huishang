d3297c60ed0a1b3b66f7cd2b1528b984ce792857
test <nnhjgg@163.com>
2017-11-01 10:32:32 +0800

    progress BUG fix

--- rjs/run.bat
+++ rjs/run.bat
@@ -1,42 +1,36 @@
 @echo off&setlocal enabledelayedexpansion
 if defined flag goto next
-title node-progress-bar  
-mode con cols=60 lines=20
-call :get_tab tab
+title n_p_bar  
+mode con cols=60 lines=12
+
 for /l %%a in (1,1,22) do set "t=!t!"
 echo,&echo,&echo,
 echo %tab%┌──────────────────────────────────┐
 echo %tab%│      READY GO ...                │ 
 echo %tab%└──────────────────────────────────┘
 echo %tab%%t%&set /p=%tab% <nul
+@echo.
 set "flag=a"
 start /b %~fs0
 :lp
-for /l %%a in (1,1,20) do (
+for /l %%a in (1,1,30) do (
    set /p=■<nul
-   for /l %%a in (1,1,500) do echo>nul
+   for /l %%a in (1,1,600) do echo>nul
+)
+for /l %%a in (1,1,30) do (
+    set /p= <nul
+    for /l %%a in (1,1,600) do echo>nul
 )
 goto lp    
 :next
 
 @cd %~dp0
-@echo on
-@echo clear...
 @node checkfolder.js > log.txt
-@echo build config...
 @node prebuild.js >> log.txt
-@echo check file...
 @node checkfile.js >> log.txt
-@echo build...
 @node r.js -o build.js >> log.txt
-@echo clear...
 @node afetrbuild.js >> log.txt
-@echo ###############################
 @echo success...
-@echo ###############################
 
-taskkill /f /fi "windowtitle eq node-progress-bar*" /im "cmd.exe">nul 2>nul
+taskkill /f /fi "windowtitle ne n_p_bar" /im "cmd.exe"
 exit
-:get_tab
-for /f "skip=39 delims=T" %%a in (%windir%\system32\MsDtc\Trace\msdtcvtr.bat) do if not defined %1 set "%1=%%a"
-call,set "%1=%%%1:~,-1%%"
